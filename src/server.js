d14943cdd314e125aa494bab9875b332d3c28c9d
Paolo Chang <chanhwan.chang@gmail.com>
2021-09-06 16:52:03 -0400

    Express: add global middleware

--- src/server.js
+++ src/server.js
@@ -4,17 +4,32 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log("This is gossip middleware");
+const loggingMiddleware = (req, res, next) => {
+  console.log(`Logging[${req.method}]: ${req.url}`);
   next();
 };
 
-app.get("/", gossipMiddleware, (req, res) => {
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not allowed!</h1>");
+  }
+  console.log("Allowed, you may continue");
+  next();
+};
+
+app.use(loggingMiddleware);
+app.use(privateMiddleware);
+app.get("/", (req, res) => {
   return res.send("<h1>This is home page</h1>");
 });
 
 app.get("/login", (req, res) => {
   return res.send("Login here");
+});
+
+app.get("/protected", (req, res) => {
+  return res.send("Welcome to the private lounge!");
 });
 
 app.listen(PORT, () =>
