5664d1355aa8e83e6c2aa63e4df5152dff24f31a
Paolo Chang <chanhwan.chang@gmail.com>
2021-09-07 21:55:23 -0400

    Routers: add routers

--- src/server.js
+++ src/server.js
@@ -5,19 +5,29 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
 app.use(logger);
-app.get("/", (req, res) => {
-  return res.send("<h1>This is home page</h1>");
-});
 
-app.get("/login", (req, res) => {
-  return res.send("Login here");
-});
+const globalRouter = express.Router();
 
-app.get("/protected", (req, res) => {
-  return res.send("Welcome to the private lounge!");
-});
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 app.listen(PORT, () =>
   console.log(`✅ Server is listening on http://localhost:${PORT} 🚀`)
