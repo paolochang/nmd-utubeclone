1efff9276a861cf5add76be641122f9f0cb440a5
Paolo Chang <chanhwan.chang@gmail.com>
2021-09-14 23:23:53 -0400

    Add Link to home

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
       {children}
       <footer>&copy; {new Date().getFullYear()} Utube</footer>
     </div>
